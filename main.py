7e0c94a4b7e0ffd35500a4557068d18b4d4d0ebc
TheEgghead27 <45245358+TheEgghead27@users.noreply.github.com>
2021-03-18 10:35:30 -0400

    expand scope of typing trigger (inconsistencies appear to be API issues)

--- main.py
+++ main.py
@@ -32,30 +32,30 @@ async def on_message(mention: discord.Message):
 
     match = re.match(r'!render (\d+)', mention.content)
     if match:
-        number = int(match.group(1))
-        if (number < 2 or number > 150):
-            await mention.channel.send(content='Number of messages must be between 2 and 150')
-            return
-        messages = []
-        async for message in mention.channel.history(limit=number, oldest_first=False, before=mention):
-            messages.insert(0, Message(message))
- 
-        thread = []
-        users_to_names = {}
-        counter = Counter()
-        for message in messages:
-            thread.append(Comment(message))
-            users_to_names[message.user.id] = message.user.name
-            counter.update({message.user.id: 1})
-        if (len(users_to_names) >= 2): 
-            most_common = [users_to_names[t[0]] for t in counter.most_common()]
-            characters = anim.get_characters(most_common)
-            output_filename = str(mention.id) + '.mp4'
-            async with mention.channel.typing():
+        async with mention.channel.typing():
+            number = int(match.group(1))
+            if (number < 2 or number > 150):
+                await mention.channel.send(content='Number of messages must be between 2 and 150')
+                return
+            messages = []
+            async for message in mention.channel.history(limit=number, oldest_first=False, before=mention):
+                messages.insert(0, Message(message))
+     
+            thread = []
+            users_to_names = {}
+            counter = Counter()
+            for message in messages:
+                thread.append(Comment(message))
+                users_to_names[message.user.id] = message.user.name
+                counter.update({message.user.id: 1})
+            if (len(users_to_names) >= 2): 
+                most_common = [users_to_names[t[0]] for t in counter.most_common()]
+                characters = anim.get_characters(most_common)
+                output_filename = str(mention.id) + '.mp4'
                 anim.comments_to_scene(thread, characters, output_filename=output_filename)
-            await mention.channel.send(file=discord.File(output_filename))
-            os.remove(output_filename)
-        else:
-            await mention.channel.send(content='There should be at least two people in the conversation')
+                await mention.channel.send(file=discord.File(output_filename))
+                os.remove(output_filename)
+            else:
+                await mention.channel.send(content='There should be at least two people in the conversation')
 
 client.run(token)

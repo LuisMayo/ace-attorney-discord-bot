19ccd3a2320c51db4bc3f4ea0bc1694affb474c4
RemedyTwo <el.famoso.remedo@gmail.com>
2021-03-26 23:10:36 +0100

    commit

--- message.py
+++ message.py
@@ -1,18 +1,19 @@
 from discord import Message
-from discord import Emoji
+from discord import mentions
 import re
 
 class Message:
     def __init__(self, update: Message):
         self.user = User(update.author)
-        temp = update.content
-        if (re.search('<:\w{2,32}:\d{18}>', temp)):
-            temp = re.sub(r'<:\w{2,32}:\d{18}>', '(emoji)', temp)
-        if (re.search('(https?)\S*', temp)):
+        temp = update.clean_content
+        if (match := re.search('<:(\w{2,32}):\d{18}>', temp)): # emojis
+            emoji_name = match.group(1)
+            temp = re.sub(r'<:\w{2,32}:\d{18}>', ':' + emoji_name + ':', temp)
+        if (re.search('(https?)\S*', temp)): # links
             temp = re.sub(r'(https?)\S*', '(link)', update.content)
         self.text = temp
 
 class User:
     def __init__(self, user):
         self.name = user.display_name
-        self.id = user.id
+        self.id = user.id 

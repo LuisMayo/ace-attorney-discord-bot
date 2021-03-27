e3e4e60280278b0c0eec3080ee4ff5f2cf22ea85
RemedyTwo <el.famoso.remedo@gmail.com>
2021-03-27 14:43:27 +0100

    commit

--- message.py
+++ message.py
@@ -7,6 +7,17 @@ class Message:
         tmp = update.clean_content
         tmp = re.sub(r'<:\w{2,32}:\d{18}>', '', tmp) # emojis
         tmp = re.sub(r'(https?)\S*', '(link)', tmp) # links
+        for file in update.attachments: # attachments
+            if file.filename.split('.')[-1] in {'jpg', 'jpeg', 'JPG', 'JPEG', 'png', 'PNG'}:
+                tmp += ' (image)'
+            elif file.filename.split('.')[-1] in {'gif', 'gifv'}:
+                tmp += ' (gif)'
+            elif file.filename.split('.')[-1] in {'mp4', 'webm'}:
+                tmp += ' (video)'
+            elif file.filename.split('.')[-1] in {'mp3', 'wav', 'ogg'}:
+                tmp += ' (audio)'
+            else:
+                tmp += ' (file)'
         self.text = tmp
 
 class User:

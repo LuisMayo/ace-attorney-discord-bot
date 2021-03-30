5f2cc2533c7f43e7f0c8107b16ae5d3188aa6b89
RemedyTwo <40474557+RemedyTwo@users.noreply.github.com>
2021-03-30 23:34:23 +0200

    Mentions/custom emojis/attachments formatting (#11)
    Removed emojis and added more replacements like (video) or (audio) for different file types

--- main.py
+++ main.py
@@ -39,7 +39,9 @@ async def on_message(mention: discord.Message):
                 return
             messages = []
             async for message in mention.channel.history(limit=number, oldest_first=False, before=mention):
-                messages.insert(0, Message(message))
+                msg = Message(message)
+                if msg.text.strip():
+                    messages.insert(0, msg)
      
             thread = []
             users_to_names = {}
--- message.py
+++ message.py
@@ -1,10 +1,27 @@
 from discord import Message
 import re
+from emoji.core import demojize
 
 class Message:
     def __init__(self, update: Message):
         self.user = User(update.author)
-        self.text = re.sub(r'(https?)\S*', '(link)', update.content)
+        tmp = update.clean_content
+        tmp = re.sub(r'(https?)\S*', '(link)', tmp) # links
+        tmp = demojize(tmp)
+        tmp = re.sub(r'[<[a:]?]?:\w{2,32}:[\d*>]?', '', tmp) # custom static and animated emojis
+        tmp = re.sub(r'​', '', tmp) # @everyone, @here 
+        for file in update.attachments: # attachments
+            if file.filename.split('.')[-1] in {'jpg', 'jpeg', 'JPG', 'JPEG', 'png', 'PNG'}:
+                tmp += ' (image)'
+            elif file.filename.split('.')[-1] in {'gif', 'gifv'}:
+                tmp += ' (gif)'
+            elif file.filename.split('.')[-1] in {'mp4', 'webm'}:
+                tmp += ' (video)'
+            elif file.filename.split('.')[-1] in {'mp3', 'wav', 'ogg'}:
+                tmp += ' (audio)'
+            else:
+                tmp += ' (file)'
+        self.text = tmp
 
 class User:
     def __init__(self, user):

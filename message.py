0f938d1a0691cd511ae3c354980a8b11aa84d372
RemedyTwo <el.famoso.remedo@gmail.com>
2021-03-30 19:11:11 +0200

    stock emoji, everyone and here mentions fixes

--- message.py
+++ message.py
@@ -1,12 +1,16 @@
 from discord import Message
 import re
+from emoji.core import demojize
 
 class Message:
     def __init__(self, update: Message):
         self.user = User(update.author)
         tmp = update.clean_content
-        tmp = re.sub(r'<:\w{2,32}:\d{18}>', '', tmp) # emojis
+        print(tmp)
         tmp = re.sub(r'(https?)\S*', '(link)', tmp) # links
+        tmp = demojize(tmp)
+        tmp = re.sub(r'[<[a:]?]?:\w{2,32}:[\d*>]?', '', tmp) # custom static and animated emojis
+        tmp = re.sub(r'​', '', tmp) # @everyone, @here 
         for file in update.attachments: # attachments
             if file.filename.split('.')[-1] in {'jpg', 'jpeg', 'JPG', 'JPEG', 'png', 'PNG'}:
                 tmp += ' (image)'
@@ -23,4 +27,4 @@ class Message:
 class User:
     def __init__(self, user):
         self.name = user.display_name
-        self.id = user.id 
+        self.id = user.id

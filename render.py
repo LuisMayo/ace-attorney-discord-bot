1fc7d52575261515aac8ab18e8261b2711ff0518
Fabi Portillo <66618574+fsmeow@users.noreply.github.com>
2021-06-08 07:25:12 -0600

    Create render.py

---
+++ render.py
@@ -0,0 +1,44 @@
+import textwrap
+
+from discord.ext.commands import Context
+from discord.message import Message
+from enum import Enum
+from objection_engine.beans.comment import Comment
+from typing import List
+
+class State(Enum):
+    QUEUED = 0
+    INPROGRESS = 1
+    FAILED = 2
+    RENDERED = 3
+    DONE = 4
+
+class Render:
+    def __init__(self, state: State, discordContext: Context, feedbackMessage: Message, messages: List[Comment]):
+        self.state = state
+        self.discordContext = discordContext
+        self.feedbackMessage = feedbackMessage
+        self.messages = messages
+        self.outputFilename = f"{str(discordContext.message.id)}.mp4"
+
+    def getState(self):
+        return self.state
+        
+    def getContext(self):
+        return self.discordContext
+
+    def getFeedbackMessage(self):
+        return self.feedbackMessage
+
+    def getMessages(self):
+        return self.messages
+
+    def getOutputFilename(self):
+        return self.outputFilename
+
+    def setState(self, state: State):
+        self.state = state
+        
+    async def updateFeedback(self, newContent: str):
+        if self.feedbackMessage.content != textwrap.dedent(newContent):
+            await self.feedbackMessage.edit(content=textwrap.dedent(newContent))
